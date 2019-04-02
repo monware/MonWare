f26b7be944d06b23a32b966d5693579b1acaee23
Lizbeth Arguello <lizbethac@ciencias.unam.mx>
2019-04-01 21:06:27 -0600

    ColocaMarcador, EditaDatos, EliminaCuenta, EliminaMarcador

---
+++ MexiMapa/src/main/java/controlador/EliminaCuenta.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import modelo.Usuario;
+import modelo.UsuarioDAO;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+/**
+ *
+ * @author lizbethac
+ */
+@ManagedBean
+public class EliminaCuenta{
+    private int idusuario;
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+    
+    public void eliminaUsuario(){     
+        UsuarioDAO udao = new UsuarioDAO();
+        Usuario u = udao.find(idusuario);
+        if(u!=null){
+            udao.delete(u);
+        }
+        
+    }
+}
